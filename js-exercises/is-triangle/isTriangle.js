57def0fd96b430dd4c5bfc406af253b8269515d2
Vaibhav <varora@catalystone.com>
2020-03-08 17:39:22 +0530

    extract validNumbers out as separated function
    validNumbers will be helpful in coming exercises to get completed, so it's good to pull it out and reuse the potential of code

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,28 +1,4 @@
-const validNumbers = (...values) => {
-  let result = true;
-
-  if (values === undefined || values.length === undefined || values.length <= 0) {
-    result = false;
-    return result;
-  }
-
-  for (let index = 0; index < values.length; index += 1) {
-    const value = values[index];
-    if (typeof value !== 'number') {
-      result = false;
-      break;
-    }
-  }
-
-  if (!result) {
-    throw new TypeError(`
-    One of the provided values is not a number.
-    Check the values sent - ${values.toString()}
-    `);
-  }
-
-  return result;
-};
+import { validNumbers } from '../common/validNumbers';
 
 const isTriangle = (side1, side2, side3) => {
   let isValidTriangle = false;
